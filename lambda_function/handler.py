6f52d732b19be44f71c666e5ac9bcaac5af887af
smit-darji <smit.softvan@gmail.com>
2025-12-11 18:13:11 +0530

    added

--- lambda_function/handler.py
+++ lambda_function/handler.py
@@ -3,7 +3,7 @@ import json
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": json.dumps("Lambda Deployment Success - Updated!")
+        "body": json.dumps("Lambda Deployment Success - Updated!12122121221")
     }
 
 
